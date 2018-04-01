a53e69caaf16ce270e9cfa50decb96f478f5fa4d
Joel Fernandes <joel@linuxinternals.org>
2018-03-31 22:58:02 -0700

    Update
    Signed-off-by: Joel Fernandes <joel@linuxinternals.org>

--- bcc/build-bcc.sh
+++ bcc/build-bcc.sh
@@ -8,5 +8,5 @@ rm -rf build && mkdir -p build && cd build
 cmake .. -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_C_COMPILER=clang-6.0 -DCMAKE_CXX_COMPILER=clang++-6.0
 make -j4
 make install
-
-# rm -rf /bcc-master
+cd ..
+rm -rf build
