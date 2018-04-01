6f1c70f2cd6a8dab6a33c3c97d5421e30737fba6
Joel Fernandes <joel@linuxinternals.org>
2018-03-31 20:26:26 -0700

    Update
    Signed-off-by: Joel Fernandes <joel@linuxinternals.org>

--- bcc/build-bcc.sh
+++ bcc/build-bcc.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# This script should run within a bcc checkout
+
+spath=$( cd "$(dirname "$0")" ; pwd -P )
+cd $spath
 
 rm -rf build && mkdir -p build && cd build
 cmake .. -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_C_COMPILER=clang-6.0 -DCMAKE_CXX_COMPILER=clang++-6.0
