452e34b62b45f558f09236765cef55ca5e42fe94
duyuchao <yuchao.du@unisoc.com>
2019-04-30 18:17:04 +0800

    utils: add kbuildout support for out-of-tree kernel builds
    Signed-off-by: yuchao.du@unisoc.com

--- utils/packheaders.sh
+++ utils/packheaders.sh
@@ -2,7 +2,8 @@
 
 # Utility to build kernel headers tar/zip file
 # must be run from the top level of a kernel source directory
-# and supplied an output file name
+# and supplied an output file name(add supplied a kbuildout
+# full path if you need)
 
 MKTEMP=0; if [[ -z ${TDIR+x} ]]  || [[ ! -d "${TDIR}" ]]; then
 	TDIR=`mktemp -d`; MKTEMP=1; fi
@@ -10,8 +11,9 @@ rm -rf $TDIR/*
 TDIR_ABS=$( cd "$TDIR" ; pwd -P )
 
 
-if [ $# -ne 1 ]; then
-  echo "usage: makeheaders.sh <output file name>"
+if [ $# -ne 1 ] && [ $# -ne 2 ]; then
+  echo "usage: packheaders.sh <output file name>"
+  echo "usage: or packheaders.sh <output file name> <kbuildout full path>"
   exit 0
 fi
 
@@ -19,6 +21,12 @@ mkdir -p $TDIR_ABS/kernel-headers
 
 find arch -name include -type d -print | xargs -n1 -i: find : -type f -exec cp --parents {} $TDIR_ABS/kernel-headers/ \;
 find include -exec cp --parents {} $TDIR_ABS/kernel-headers/ 2> /dev/null \;
+if [ $# -eq 2 ]; then
+    cd $2/obj/KERNEL
+    find arch -name include -type d -print | xargs -n1 -i: find : -type f -exec cp --parents {} $TDIR_ABS/kernel-headers/ \;
+    find include -exec cp --parents {} $TDIR_ABS/kernel-headers/ 2> /dev/null \;
+    cd -
+fi
 tar -zcf $1 --directory=$TDIR_ABS kernel-headers
 
 zip -r $1.zip $1
