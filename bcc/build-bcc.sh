0cf0a15aef13c14cf1e5604ffe52e198c0958612
Joel Fernandes <joelaf@google.com>
2019-07-11 14:17:30 -0400

    New release
    Signed-off-by: Joel Fernandes <joelaf@google.com>

--- bcc/build-bcc.sh
+++ bcc/build-bcc.sh
@@ -5,7 +5,7 @@ spath=$( cd "$(dirname "$0")" ; pwd -P )
 cd $spath
 
 rm -rf build && mkdir -p build && cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_C_COMPILER=clang-6.0 -DCMAKE_CXX_COMPILER=clang++-6.0
+cmake .. -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_C_COMPILER=clang-7 -DCMAKE_CXX_COMPILER=clang++-7
 make -j4
 make install
 cd ..
