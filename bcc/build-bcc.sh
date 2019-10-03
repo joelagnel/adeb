93adaae59cfcea9368db9adb068966f37a72e09b
Terry Chia <terrycwk1994@gmail.com>
2019-10-03 21:21:01 +0800

    Add bcc for python 3.

--- bcc/build-bcc.sh
+++ bcc/build-bcc.sh
@@ -8,5 +8,10 @@ rm -rf build && mkdir -p build && cd build
 cmake .. -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_C_COMPILER=clang-7 -DCMAKE_CXX_COMPILER=clang++-7
 make -j4
 make install
+cmake -DPYTHON_CMD=python3 ..
+pushd src/python/
+make
+make install
+popd
 cd ..
 rm -rf build
